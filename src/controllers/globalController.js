42a1f41ee70fb0709a85c8adc2f51fd252a41843
yjglab <yjgdesign@gmail.com>
2022-04-15 16:05:33 +0900

    British Museum titles 정리 및 출력

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,49 +1,51 @@
-let britishMsmDataList;
-const britishMsmData = (britishMsmDataString) => {
-  const info = {
-    titles: "",
+const spawn = require("child_process").spawn;
+const result = spawn("python", ["infoCrawler.py"]);
+
+result.stdout.on("data", function (data) {
+  console.log(data.toString());
+  handleBritishMsmData(data);
+});
+result.stderr.on("data", function (data) {
+  console.log(data.toString());
+});
+
+let britishMsmInfo = {};
+
+const handleBritishMsmData = (data) => {
+  const dataStringList = [];
+  let britishMsmDataString = data.toString();
+
+  let startIdx = 0;
+  let idxOfFilter = 0;
+  const FILTER = "FILTER";
+  while (idxOfFilter !== -1) {
+    idxOfFilter = britishMsmDataString.indexOf(`${FILTER}`, startIdx);
+    let dataString = britishMsmDataString.slice(startIdx, idxOfFilter); // 가공안된거
+    dataStringList.push(dataString);
+    startIdx = idxOfFilter + `${FILTER}}`.length;
+  }
+
+  for (let i = 0; i < dataStringList.length; i += 1) {
+    dataStringList[i] = dataStringList[i].split("//");
+    for (let j = 0; j < dataStringList[i].length; j += 1) {
+      dataStringList[i][j] = dataStringList[i][j].replace(/(\r\n\r\n)/gm, "");
+    }
+    dataStringList[i] = dataStringList[i].filter((el) => el !== "");
+  }
+
+  britishMsmInfo = {
+    titles: dataStringList[0],
+    dates: dataStringList[1],
   };
-};
-let arr = [];
-const infoCrawler = () => {
-  const spawn = require("child_process").spawn;
-  const result = spawn("python", ["infoCrawler.py"]);
-
-  result.stdout.on("data", function (data) {
-    console.log(data.toString());
-    let britishMsmDataString = data.toString();
-
-    let startIdx = 0;
-    let idxOfFilter = 0;
-    const FILTER = "FILTER";
-    while (idxOfFilter !== -1) {
-      idxOfFilter = britishMsmDataString.indexOf(`${FILTER}`, startIdx);
-      let dataString = britishMsmDataString.slice(startIdx, idxOfFilter);
-      arr.push(dataString);
-      startIdx = idxOfFilter + `${FILTER}}`.length;
-    }
-
-    console.log("fffff");
-    for (let i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[i].split("//");
-      for (let j = 0; j < arr[i].length; j += 1) {
-        arr[i][j] = arr[i][j].replace(/(\r\n\r\n)/gm, "");
-      }
-      arr[i] = arr[i].filter((el) => el !== "");
-    }
-
-    console.log(arr[0]);
-    console.log(arr[1]);
-    // console.log(data.toString());
-    // console.log(britishMsmDataList);
-  });
-  result.stderr.on("data", function (data) {
-    console.log(data.toString());
-  });
+  console.log(britishMsmInfo.titles);
 };
 
 export const main = (req, res) => {
-  res.render("main", { pageTitle: "결과", dataList: arr[1] });
+  res.render("main", {
+    pageTitle: "결과",
+    britishMsmInfo: {
+      titles: britishMsmInfo.titles,
+      dates: britishMsmInfo.dates,
+    },
+  });
 };
-
-infoCrawler();
