6eb11f24ce47cf79e202106b88f3e04456ec9665
yjglab <yjgdesign@gmail.com>
2022-04-20 03:00:23 +0900

    pompidou 동적 정보 스크롤링 pass

--- src/pydata/infoCrawler.py
+++ src/pydata/infoCrawler.py
@@ -33,45 +33,27 @@ def print_msm_data(url, titles_selector, dates_selector):
     exibition_titles = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, titles_selector)))
     exibition_dates = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, dates_selector)))
     # exn_info = set_selector(driver, titles_selector, dates_selector).copy()
-    
+   
     if url == 'https://www.britishmuseum.org/exhibitions-events': 
         # british museum 정보 중 전시정보가 아닌 것은 제외함
         exibition_titles = exibition_titles[:13]
     if url == 'https://www.centrepompidou.fr/en/':
+        pass
         # pompidou museum 모든 전시정보 가져오기 위해서 별도 처리
-        # time.sleep(1)
-        selector = driver.find_element(by=By.CSS_SELECTOR, value="#page-content > div > div.owl-carousel.cp-carousel.carousel-event-homepage.owl-theme.owl-loaded.owl-drag > div.owl-nav > button.owl-next")
-        selector.click()
-        titles_temp, dates_temp = [], []
-        exibition_titles.extend(WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, titles_selector))))
-        exibition_dates.extend(WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, dates_selector))))
+        # time.sleep() 하니까 원본 exibition_** 변수내용이 없어져버림..?
+        # selector = driver.find_element(by=By.CSS_SELECTOR, value="#page-content > div > div.owl-carousel.cp-carousel.carousel-event-homepage.owl-theme.owl-loaded.owl-drag > div.owl-nav > button.owl-next")
+        # selector.click()
         
+        # titles_temp, dates_temp = [], []
+        # exibition_titles.extend(WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, titles_selector))))
+        # exibition_dates.extend(WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, dates_selector))))
         
-        for title_webElement in exibition_titles:
-            if title_webElement not in titles_temp:
-                titles_temp.append(title_webElement)
-        for date_webElement in exibition_dates:
-            if date_webElement not in dates_temp:
-                dates_temp.append(date_webElement)
-        exibition_titles = titles_temp[:]
-        exibition_dates = dates_temp[:]
-        # re_titles = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, titles_selector)))
-        # re_dates = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, dates_selector)))
-        
-        # exibition_titles.extend(re_titles)
-        # exibition_dates.extend(re_dates)
-
-        # titles_temp = []
-        # dates_temp = []
-        
-        # for title in exibition_titles:
-        #     if title not in titles_temp:
-        #         titles_temp.append(title)
-
-        # for date in exibition_dates:
-        #     if date not in dates_temp:
-        #         dates_temp.append(date)
-        
+        # for title_webElement in exibition_titles:
+        #     if title_webElement not in titles_temp:
+        #         titles_temp.append(title_webElement)
+        # for date_webElement in exibition_dates:
+        #     if date_webElement not in dates_temp:
+        #         dates_temp.append(date_webElement)
         # exibition_titles = titles_temp[:]
         # exibition_dates = dates_temp[:]
     
