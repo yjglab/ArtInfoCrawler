9d64eead78db954acaad6de22dd21b74469a5659
yjg <yjglab@yjg.local>
2022-08-13 00:46:39 +0900

    레이지로드 부드럽게 처리

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -32,11 +32,6 @@ $(function () {
         return i % 8; // 3
       },
     },
-    // callbacks: {
-    //   onMixFail: function (state) {
-    //     console.log("No items could be found matching the requested filter");
-    //   },
-    // },
   });
 
   var inputText;
@@ -88,3 +83,21 @@ $(function () {
 });
 
 //
+window.addEventListener("load", function () {
+  setTimeout(lazyLoad, 1000);
+});
+
+function lazyLoad() {
+  var card_images = document.querySelectorAll(".card-front");
+
+  card_images.forEach(function (card_image) {
+    // var image_url = card_image.getAttribute('data-image-full');
+    var content_image = card_image.querySelector("img");
+    var image_url = content_image.getAttribute("src");
+    content_image.src = image_url;
+    content_image.addEventListener("load", function () {
+      card_image.style.backgroundImage = "url(" + image_url + ")";
+      card_image.className = card_image.className + " is-loaded";
+    });
+  });
+}
