98e3427c37d0556184d507a557d114790c95860a
yjglab <yjgdesign@gmail.com>
2022-09-16 21:30:11 +0900

    port add

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "dotenv/config";
 
 import app from "./index";
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🔵 PORT${PORT} 연결`);
 });
