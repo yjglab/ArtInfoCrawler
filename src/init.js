aa7277daa740829974a174b546ea364549b6269c
yjglab <yjgdesign@gmail.com>
2022-09-16 22:37:16 +0900

    ..

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import "./db";
 import "./models/ExbModel";
 
 import app from "./index";
-console.log(process.env.DB_URL);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🔵 PORT${PORT} 연결`);
