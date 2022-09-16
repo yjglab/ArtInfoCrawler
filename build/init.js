942271d928a9cba65a92961a6c8e453f78cc6afb
yjglab <yjgdesign@gmail.com>
2022-09-16 22:28:43 +0900

    ..

--- build/init.js
+++ build/init.js
@@ -1,12 +1,17 @@
 "use strict";
 
+require("dotenv/config");
+
 require("./db");
+
+require("./models/ExbModel");
 
 var _index = _interopRequireDefault(require("./index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var PORT = 8080;
+console.log(process.env.DB_URL);
+var PORT = process.env.PORT || 8080;
 
 _index["default"].listen(PORT, function () {
   console.log("\uD83D\uDD35 PORT".concat(PORT, " \uC5F0\uACB0"));
