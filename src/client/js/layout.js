011f223e5eaf163556462896648dce31558b7e53
yjglab <yjgdesign@gmail.com>
2022-09-30 15:43:11 +0900

    일부 코드 수정

--- src/client/js/layout.js
+++ src/client/js/layout.js
@@ -7,6 +7,10 @@
   const r = require.context("../videos", false, /\.(mp4)$/);
   return r.keys().map(r);
 })();
+// (function importAllFonts() {
+//   const r = require.context("../fonts", false, /\.(woff|woff2|eot|ttf|otf)$/);
+//   return r.keys().map(r);
+// })();
 
 // 사용자 마우스 잠수 탐지
 let detectionCount = 0;
--- src/client/js/nav.js
+++ src/client/js/nav.js
@@ -1,59 +1,65 @@
-if (document.title.toLowerCase().indexOf("main") === -1) {
-  document
-    .querySelectorAll(".nav__item-filter .nav__item-content-gridbox")
-    .forEach((v) => (v.style.display = "none"));
-  document.querySelector(".nav__item-content-box.triple").style.height = "87px";
-}
-
-const $mainBannerSectionContainer = document.querySelector(
-  ".main-banner-section-container"
-);
-const $filterContainerBackground = document.querySelector(".filter-container");
+// intro 페이지 nav
 const $navContainer = document.querySelector(".nav-container");
-const $filterSearchbar = document.querySelector(".filter-search-bar");
 $(window).scroll(function () {
   if ($(document).scrollTop() > 120) {
-    $mainBannerSectionContainer.classList.add("hide");
     $navContainer.classList.add("scroll-down");
   } else {
-    $mainBannerSectionContainer.classList.remove("hide");
     $navContainer.classList.remove("scroll-down");
-    // $(".main-card-mix-container").mixItUp("filter", "all");
-    document.querySelectorAll(".filter-list-category .filter").forEach((v) => {
-      if (v.classList.contains("option-all")) v.classList.add("option-clicked");
-      else {
-        v.classList.remove("option-clicked");
-      }
-    });
-  }
-});
-$(window).scroll(function () {
-  if ($(document).scrollTop() > 200) {
-    $(".update-info-container").fadeOut();
-  } else {
-    $(".update-info-container").fadeIn();
   }
 });
 
-// 업데이트 정보 nav 플로팅
-const $navListContainer = document.querySelector(".nav-list-container");
-const $LastUpdated = $navListContainer.querySelector(".last-updated");
-const $exbCount = $navListContainer.querySelector(".exb-count");
-let today = new Date();
-$LastUpdated.textContent =
-  "Last Updated on " +
-  today.getFullYear() +
-  ". " +
-  `${today.getMonth() + 1}` +
-  ". " +
-  today.getDate();
-$exbCount.textContent =
-  document.querySelectorAll(".main-card-container").length + " Exhibitions";
+// main 페이지 nav
+if (document.title.toLowerCase().indexOf("introduction") === -1) {
+  const $mainBannerSectionContainer = document.querySelector(
+    ".main-banner-section-container"
+  );
 
-const $updateSpinnerIcon = document.querySelector(".update-spinner i");
-// let randomNum = Math.floor(Math.random() * 15);
-setTimeout(() => {
-  $updateSpinnerIcon.classList.remove("fa-circle-notch");
-  $updateSpinnerIcon.classList.add("fa-check");
-  $updateSpinnerIcon.classList.add("update-completed");
-}, Math.floor(Math.random() * 20) * 1000 + 15000);
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 120) {
+      $mainBannerSectionContainer.classList.add("hide");
+    } else {
+      $mainBannerSectionContainer.classList.remove("hide");
+      // $(".main-card-mix-container").mixItUp("filter", "all");
+      document
+        .querySelectorAll(".filter-list-category .filter")
+        .forEach((v) => {
+          if (v.classList.contains("option-all"))
+            v.classList.add("option-clicked");
+          else {
+            v.classList.remove("option-clicked");
+          }
+        });
+    }
+  });
+
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 200) {
+      $(".update-info-container").fadeOut();
+    } else {
+      $(".update-info-container").fadeIn();
+    }
+  });
+
+  // 업데이트 정보 nav 플로팅
+  const $navListContainer = document.querySelector(".nav-list-container");
+  const $LastUpdated = $navListContainer.querySelector(".last-updated");
+  const $exbCount = $navListContainer.querySelector(".exb-count");
+  let today = new Date();
+  $LastUpdated.textContent =
+    "Last Updated on " +
+    today.getFullYear() +
+    ". " +
+    `${today.getMonth() + 1}` +
+    ". " +
+    today.getDate();
+  $exbCount.textContent =
+    document.querySelectorAll(".main-card-container").length + " Exhibitions";
+
+  const $updateSpinnerIcon = document.querySelector(".update-spinner i");
+  // let randomNum = Math.floor(Math.random() * 15);
+  setTimeout(() => {
+    $updateSpinnerIcon.classList.remove("fa-circle-notch");
+    $updateSpinnerIcon.classList.add("fa-check");
+    $updateSpinnerIcon.classList.add("update-completed");
+  }, Math.floor(Math.random() * 20) * 1000 + 15000);
+}
